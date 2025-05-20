2bf62c6924f8df3737d620d6faa57238ff1887ed
Hatim Alouani <haalouan@e2r7p6.1337.ma>
2025-05-20 19:53:30 +0100

    push ana

--- php/index.php
+++ php/index.php
@@ -1,2 +1,27 @@
-<!-- ../php/index.php -->
-<?php phpinfo(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <lable> quantity:</lable>
+        <input type="text" name ="quantity">
+        <input type="submit" value="total">
+    </form>
+</body>
+</html>
+
+
+<?php
+    $item = "piza";
+    $price = 10;
+    $quantity = $_POST['quantity'];
+    $total = null;
+
+    $total = $price * $quantity;
+    echo "The total price for $quantity $item is $total";
+?>
