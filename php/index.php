d70bdd2889d5cdaa26f50e7ef78e58a9f42510f3
Hatim Alouani <haalouan@e2r7p6.1337.ma>
2025-05-26 18:12:59 +0100

    push

--- php/index.php
+++ php/index.php
@@ -7,21 +7,30 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="index.php" method="post">
-        <lable> quantity:</lable>
-        <input type="text" name ="quantity">
-        <input type="submit" value="total">
-    </form>
+    <?php
+        echo "hello , today is " . date("d/m/y") ;
+        $color = "red";
+        $x = $y = $z = "fruit";
+        $txt1 = "Learn PHP";
+        $txt2 = "FPK university";
+        function pr($txt1, $txt2){
+        // echo , Echo, ECHO
+        
+        echo "<p><h2> $txt1 at $txt2 </h2></p>";
+        echo '<p> Study PHP at '. $txt2 .'</p>';
+        }
+        pr($txt1, $txt2);
+        function saluer($nom = "utilisateur") {
+            echo "Bonjour, $nom !<br>";
+           }
+        saluer(); // Affichera "Bonjour, utilisateur !"
+        saluer("Alice"); // Affichera "Bonjour, Alice !"
+        function somme(...$nombres) {
+            return array_sum($nombres);
+           }
+        echo somme(2, 4, 6); // Affichera 12
+        
+    ?>
 </body>
 </html>
 
-
-<?php
-    $item = "piza";
-    $price = 10;
-    $quantity = $_POST['quantity'];
-    $total = null;
-
-    $total = $price * $quantity;
-    echo "The total price for $quantity $item is $total";
-?>
