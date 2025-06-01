ed34f0b65f00fbd06ed3b846fc0cb8bca545af57
Hatim Alouani <haalouan@e2r7p6.1337.ma>
2025-06-01 12:18:59 +0100

    push

--- php/index.php
+++ php/index.php
@@ -1,5 +1,119 @@
+<?php
+setcookie("utilisateur", "Hatim", time() + (86400 * 30), "/", true, true);
+if (isset($__COOKIE["utilisateur"])){
+    echo "Hello, " . $__COOKIE["utilisateur"] . "<br>";
+    }
+else {
+    echo "not found <br>";
+    }
 
-<!DOCTYPE html>
+    
+        // echo "hello , today is " . date("d/m/y") ;
+        // $color = "red";
+        // $x = $y = $z = "fruit";
+        // $txt1 = "Learn PHP";
+        // $txt2 = "FPK university";
+        // function pr($txt1, $txt2){
+        // // echo , Echo, ECHO
+        
+        // echo "<p><h2> $txt1 at $txt2 </h2></p>";
+        // echo '<p> Study PHP at '. $txt2 .'</p>';
+        // }
+        // pr($txt1, $txt2);
+        // function saluer($nom = "utilisateur") {
+        //     echo "Bonjour, $nom !<br>";
+        //    }
+        // saluer(); // Affichera "Bonjour, utilisateur !"
+        // saluer("Alice"); // Affichera "Bonjour, Alice !"
+        // function somme(...$nombres) {
+        //     return array_sum($nombres);
+        //    }
+        // echo somme(2, 4, 6); // Affichera 12
+        // switch(expression){
+        //     case label1:
+        //         //
+        //         break;
+        //     default:
+        //         //
+        // }
+        // $text = "hello hatim is my name and im a student";
+        // echo strlen($text) . "<br>";
+        // echo str_word_count($text) . "<br>";
+        // // echo strrev($text) . "<br>";
+        // echo str_replace("hatim", "alouani", $text) . "<br>";
+        // echo strtoupper($text) . "<br>";
+        // echo strtolower($text) . "<br>";
+        // echo trim($text) . "<br>";
+
+        // $a = 5.5;
+        // echo var_dump($a). "<br>";
+        // echo is_int($a) . "<br>";
+        // echo is_float($a) . "<br>";
+        // echo is_finite($a) . "<br>";
+        // echo is_infinite($a) . "<br>";
+        // echo is_numeric($a) . "<br>";
+        // echo intval($a) . "<br>";
+        // $int_a = (int)$a;
+        // echo $int_a . "<br>";
+        // echo PHP_INT_MAX . "<br>" . PHP_INT_MIN . "<br>" . PHP_FLOAT_MAX . "<br>" . PHP_FLOAT_MIN . "<br>";
+        // $array_a = (array)$a;
+        // var_dump($array_a);
+        // echo "<br>";
+        // $object_a = (object)$a;
+        // var_dump($object_a);
+        // echo "<br>";
+        // echo pi(). "<br>";
+        // echo min(0, 2 , 6). "<br>";
+        // echo max(0, 2 , 6). "<br>";
+        // echo abs(-6). "<br>";
+        // echo sqrt(56). "<br>";
+        // __CLASS__ Retourne le nom de la classe lorsqu’elle est utilisée à l’intérieur d’une classe.
+        // __DIR__ Donne le chemin du répertoire du fichier en cours.
+        // __FILE__ Retourne le chemin complet du fichier en cours.
+        // __FUNCTION__ Donne le nom de la fonction lorsqu’elle est utilisée à l’intérieur d’une fonction.
+        // __LINE__ Retourne le numéro de la ligne courante dans le fichier.
+        // __METHOD__ Donne le nom de la méthode (classe et fonction) lorsqu’elle est utilisée dans une méthode.
+        // __NAMESPACE__ Donne le nom de l’espace de noms lorsqu’elle est utilisée à l’intérieur de cet espace.
+        // __TRAIT__ Retourne le nom du trait lorsqu’elle est utilisée à l’intérieur d’un trait.
+        // ClassName::class Retourne le nom de la classe spécifiée, accompagné de l’espace de noms si présent
+        // $cars = array("Volvo", "BMW", "Toyota");
+        // // $cars = [];
+        // // $cars[0] = "Volvo";
+        // // $cars[1] = "BMW";
+        // // echo $cars[0];
+
+        // $car = ["brand" => "Ford", "model" => "Mustang"];
+        // // echo $car["model"];
+        // $car[1] = "new";
+        // $car["year"] = 2024;
+        // foreach ($cars as &$value) {
+        //     $value = "Updated";
+        //    }
+        // // print($cars[1]);
+        // unset($value); 
+        // $car["color"] = "Red";
+        // array_push($cars, "Audi", "Porsche");
+        // unset($cars[1]);
+        // array_splice($cars, 1, 1);
+        // foreach ($car as $key => $value) {
+        //     echo "$key : $value<br>";
+        //    }
+
+        // setcookie("utilisateur", "Hatim", time() + (86400 * 30), "/");
+        // if (isset($__COOKIE["utilisateur"])){
+        //     echo "Hello, " . $__COOKIE["utilisateur"] . "<br>";
+        // }
+        // else {
+        //     echo "not found <br>";
+        // }
+        // setcookie("utilisateur", "Hatim-alouani", time() + (86400 * 30), "/");
+        // setcookie("utilisateur", "", time() - 3600 , "/");
+        // echo "hey <br>";
+        
+
+        
+    ?>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,30 +121,5 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        echo "hello , today is " . date("d/m/y") ;
-        $color = "red";
-        $x = $y = $z = "fruit";
-        $txt1 = "Learn PHP";
-        $txt2 = "FPK university";
-        function pr($txt1, $txt2){
-        // echo , Echo, ECHO
-        
-        echo "<p><h2> $txt1 at $txt2 </h2></p>";
-        echo '<p> Study PHP at '. $txt2 .'</p>';
-        }
-        pr($txt1, $txt2);
-        function saluer($nom = "utilisateur") {
-            echo "Bonjour, $nom !<br>";
-           }
-        saluer(); // Affichera "Bonjour, utilisateur !"
-        saluer("Alice"); // Affichera "Bonjour, Alice !"
-        function somme(...$nombres) {
-            return array_sum($nombres);
-           }
-        echo somme(2, 4, 6); // Affichera 12
-        
-    ?>
 </body>
-</html>
-
+</html>  -->
